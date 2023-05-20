1e2531171a93e4fc66f0901acb43da92a6487ad0
Stephane-boguhe <boguhestephane97@gmail.com>
2023-05-20 12:15:24 +0100

    update footer background

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="relative bg-blueGray-50 pt-8 pb-6 mt-2">
+      <footer className="relative bg-gray-100 pt-8 pb-6 mt-2">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-6/12 px-4 mx-auto text-center">
