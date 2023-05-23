e5f28d7d6da1aa70940887d2d44e498ee67f5973
ange stephane <boguhestephane97@gmail.com>
2023-05-23 16:11:00 +0100

    add admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import AdoptedPetContext from './utility/adoptedPetContext';
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./utility/adoptedPetContext";
 
-import DetailErrorBoundary from './components/detail/Pet_detail';
+import DetailErrorBoundary from "./components/detail/Pet_detail";
 
-import Home from './pages/Home/Home';
+import Home from "./pages/Home/Home";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/footer';
-import Recherche from './pages/Show-pet/Recherche';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/footer";
+import Recherche from "./pages/Show-pet/Recherche";
+import Admin from "./pages/Admin/Admin";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,6 +36,7 @@ const App = () => {
               <Route path="/" element={<Home />} />
               <Route path="/details/:id" element={<DetailErrorBoundary />} />
               <Route path="/animal/recherche" element={<Recherche />} />
+              <Route path="/admin" element={<Admin />} />
             </Routes>
             <Footer />
           </div>
@@ -44,6 +46,6 @@ const App = () => {
   );
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,8 @@
+const Admin = () => {
+  return (
+    <>
+      <h2>Hello from admin </h2>
+    </>
+  );
+};
+export default Admin;
